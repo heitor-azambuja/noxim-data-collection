dbae54d057c517ac1cb2d6d9b9656aa0b41e4f50
Heitor Azambuja <heitortazamba@gmail.com>
2023-05-09 10:37:36 -0300

    Plot throughput graph

--- plot_noxim_data.py
+++ plot_noxim_data.py
@@ -11,18 +11,35 @@ if __name__ == '__main__':
     df = pd.read_csv(args.file)
     routes = df['route'].unique()
     pirs = df['pir'].unique()
+    
+    plt.subplot(1,2,1)
     for route in routes:
-        delay_pir = df.loc[df['route'] == route, ['global_avg_delay_cycles', 'pir']]#.to_numpy()
+        delay_pir = df.loc[df['route'] == route, ['global_avg_delay_cycles', 'pir']]
         delay = []
         #  Average values that have the same route and pir
         for pir in pirs:
             delays = delay_pir.loc[delay_pir['pir'] == pir, 'global_avg_delay_cycles'].to_numpy()
             delay.append(np.mean(delays))
-        plt.plot(pirs, delay, label=route)
+        plt.plot(pirs, delay, label=route.replace('_', ' ').lower())
+    plt.grid()
     plt.legend(title='Routing Technique:')
     plt.xlabel('Packet injection rate (flit/cycle/tile)')
     plt.ylabel('Average delay (cycle)')
-    plt.title('Delay vs Locality')
+    plt.title('Delay')
+
+    plt.subplot(1,2,2)
+    for route in routes:
+        ipt_pir = df.loc[df['route'] == route, ['avg_ip_throuput', 'pir']]
+
+        ipt = []
+        for pir in pirs:
+            ipts = ipt_pir.loc[ipt_pir['pir'] == pir, 'avg_ip_throuput'].to_numpy()
+            ipt.append(np.mean(ipts))
+        plt.plot(pirs, ipt, label=route.replace('_', ' ').lower())
+    plt.grid()
+    plt.legend(title='Routing Technique:')
+    plt.xlabel('Packet injection rate (flit/cycle/tile)')
+    plt.ylabel('Average IP throughput (flit/cycle/tile)')
+    plt.title('Packet Throughput')
+    # plt.tight_layout()
     plt.show()
-
-
